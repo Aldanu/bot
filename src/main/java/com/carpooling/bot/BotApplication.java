cc81cc501572bfd895c9297d5046dde16193f6a6
Aldair <aldair.flores.moya@gmail.com>
2019-09-29 16:07:09 -0400

    Adding the class for the control of the bot and dependencies

--- src/main/java/com/carpooling/bot/BotApplication.java
+++ src/main/java/com/carpooling/bot/BotApplication.java
@@ -2,10 +2,11 @@ package com.carpooling.bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.ApiContextInitializer;
 
 @SpringBootApplication
 public class BotApplication {
-
+	//ApiContextInitializer.init();
 	public static void main(String[] args) {
 		SpringApplication.run(BotApplication.class, args);
 	}
---
+++ src/main/java/com/carpooling/bot/CarpoolingBot.java
@@ -0,0 +1,28 @@
+package com.carpooling.bot;
+
+import org.telegram.telegrambots.bots.TelegramWebhookBot;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class CarpoolingBot  extends TelegramWebhookBot {
+    @Override
+    public BotApiMethod onWebhookUpdateReceived(Update update) {
+        return null;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
+
+    @Override
+    public String getBotPath() {
+        return null;
+    }
+    //Bot body.
+}
