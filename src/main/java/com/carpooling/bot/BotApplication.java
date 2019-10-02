6dcb81b866f11e6278086f1329428e20b25e7c1f
Aldair <aldair.flores.moya@gmail.com>
2019-10-01 22:59:24 -0400

    Add function and connection to the Telegram Bot

--- src/main/java/com/carpooling/bot/BotApplication.java
+++ src/main/java/com/carpooling/bot/BotApplication.java
@@ -4,14 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @SpringBootApplication
 public class BotApplication {
 
 	public static void main(String[] args) {
 		ApiContextInitializer.init();
-		//CarpoolingBot bot = new CarpoolingBot("949452837:AAHuj-LyPG9VHc1vxIoGRJ7iMIyOoemEXHk");
 		SpringApplication.run(BotApplication.class, args);
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        try {
+            botsApi.registerBot(new CarpoolingBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
 	}
 
 }
--- src/main/java/com/carpooling/bot/CarpoolingBot.java
+++ src/main/java/com/carpooling/bot/CarpoolingBot.java
@@ -2,22 +2,34 @@ package com.carpooling.bot;
 
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class CarpoolingBot  extends TelegramWebhookBot {
     @Override
     public BotApiMethod onWebhookUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+                    .setChatId(update.getMessage().getChatId())
+                    .setText(update.getMessage().getText());
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
         return null;
     }
 
     @Override
     public String getBotUsername() {
-        return null;
+        return "TheCarpoolerBot";
     }
 
     @Override
     public String getBotToken() {
-        return null;
+        return "949452837:AAHuj-LyPG9VHc1vxIoGRJ7iMIyOoemEXHk";
     }
 
     @Override
