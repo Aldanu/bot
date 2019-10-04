600eb7f8fc49c50d45c6a082aa86469c616b3946
Aldair <aldair.flores.moya@gmail.com>
2019-10-04 18:02:05 -0400

    Change to longPooling and adding its functions

--- src/main/java/com/carpooling/bot/CarpoolingBot.java
+++ src/main/java/com/carpooling/bot/CarpoolingBot.java
@@ -1,25 +1,30 @@
 package com.carpooling.bot;
 
-import org.telegram.telegrambots.bots.TelegramWebhookBot;
-import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-public class CarpoolingBot  extends TelegramWebhookBot {
+public class CarpoolingBot  extends TelegramLongPollingBot {
+
     @Override
-    public BotApiMethod onWebhookUpdateReceived(Update update) {
-        if (update.hasMessage() && update.getMessage().hasText()) {
-            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
-                    .setChatId(update.getMessage().getChatId())
-                    .setText(update.getMessage().getText());
-            try {
-                execute(message); // Call method to send the message
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
+    public void onUpdateReceived(Update update) {
+        if(update.hasMessage()){
+            Message message = update.getMessage();
+
+            if(message.hasText()){
+
+                SendMessage sendMessageRequest = new SendMessage();
+                sendMessageRequest.setChatId(message.getChatId().toString());
+                sendMessageRequest.setText("you said: " + message.getText());
+                /*try {
+                    sendMessage(sendMessageRequest);
+                } catch (TelegramApiException e) {
+
+                }*/
             }
         }
-        return null;
     }
 
     @Override
@@ -30,10 +35,5 @@ public class CarpoolingBot  extends TelegramWebhookBot {
     @Override
     public String getBotToken() {
         return "949452837:AAHuj-LyPG9VHc1vxIoGRJ7iMIyOoemEXHk";
-    }
-
-    @Override
-    public String getBotPath() {
-        return null;
     }
 }
