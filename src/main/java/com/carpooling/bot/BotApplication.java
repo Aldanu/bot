6906ea6cd445168df0da4f02e782854aa1ca002d
Aldair <aldair.flores.moya@gmail.com>
2019-10-05 09:40:08 -0400

    Correcting functioning

--- src/main/java/com/carpooling/bot/BotApplication.java
+++ src/main/java/com/carpooling/bot/BotApplication.java
@@ -1,25 +1,21 @@
 package com.carpooling.bot;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
 import org.telegram.telegrambots.ApiContextInitializer;
-import org.telegram.telegrambots.bots.TelegramWebhookBot;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @SpringBootApplication
 public class BotApplication {
 
-	public static void main(String[] args) {
-		ApiContextInitializer.init();
-		SpringApplication.run(BotApplication.class, args);
-        TelegramBotsApi botsApi = new TelegramBotsApi();
+    public static void main(String[] args) {
+        //Add this line to initialize bots context
+        ApiContextInitializer.init();
 
-        try {
-            botsApi.registerBot(new CarpoolingBot());
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
-	}
+        SpringApplication.run(CarpoolingBot.class, args);
 
+    }
 }
--- src/main/java/com/carpooling/bot/CarpoolingBot.java
+++ src/main/java/com/carpooling/bot/CarpoolingBot.java
@@ -1,28 +1,28 @@
 package com.carpooling.bot;
 
+import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-public class CarpoolingBot  extends TelegramLongPollingBot {
+@Component
+public class CarpoolingBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        if(update.hasMessage()){
-            Message message = update.getMessage();
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            // Set variables
+            String message_text = update.getMessage().getText();
+            long chat_id = update.getMessage().getChatId();
 
-            if(message.hasText()){
-
-                SendMessage sendMessageRequest = new SendMessage();
-                sendMessageRequest.setChatId(message.getChatId().toString());
-                sendMessageRequest.setText("you said: " + message.getText());
-                /*try {
-                    sendMessage(sendMessageRequest);
-                } catch (TelegramApiException e) {
-
-                }*/
+            SendMessage message = new SendMessage() // Create a message object object
+                    .setChatId(chat_id)
+                    .setText(message_text);
+            try {
+                execute(message); // Sending our message object to user
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
             }
         }
     }
